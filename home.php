fc976109ddac79bbd48877de39bc518b770745bd
Rhey Victor Macayran <rvsmacayran@gmail.com>
2024-12-01 08:31:33 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- fonr awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     
+     <!-- cusrom css file link -->
+      <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="home">
+    <img src="image/homebg.png" alt="">
+            <!-- header section starts -->
+     <section class="header">
+        <a href="home.php" class="logo">
+            <img src="image/logo.png" alt=""></span>
+        </a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="portfolio.php">portfolio</a>
+            <a href="conatct.php">contact</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+     </section>
+     
+    </div>
+
+    <!-- header section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+ <script src="js/script.js"></script>
+
+    
+</body>
+</html>
